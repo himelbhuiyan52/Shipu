e35afe78a8a60e757552378975abb873a9c8f1ca
himelbhuiyan52 <himelbhuiyan52@gmail.com>
2025-06-22 02:59:53 +0600

    Update adminonly.js via Messenger Goat Bot

--- scripts/cmds/adminonly.js
+++ scripts/cmds/adminonly.js
@@ -1,51 +1,51 @@
-const fs = require("fs-extra");
-const { config } = global.GoatBot;
-const { client } = global;
-
 module.exports = {
 	config: {
 		name: "adminonly",
-		aliases: ["adonly", "onlyad", "onlyadmin"],
-		version: "1.5",
+		aliases: ["onlyadbox", "adboxonly", "adminboxonly"],
+		version: "1.3",
 		author: "NTKhang",
 		countDown: 5,
-		role: 2,
+		role: 1,
 		description: {
-			vi: "bật/tắt chế độ chỉ admin mới có thể sử dụng bot",
-			en: "turn on/off only admin can use bot"
+			vi: "bật/tắt chế độ chỉ quản trị của viên nhóm mới có thể sử dụng bot",
+			en: "turn on/off only admin box can use bot"
 		},
-		category: "owner",
+		category: "box chat",
 		guide: {
-			vi: "   {pn} [on | off]: bật/tắt chế độ chỉ admin mới có thể sử dụng bot"
-				+ "\n   {pn} noti [on | off]: bật/tắt thông báo khi người dùng không phải là admin sử dụng bot",
-			en: "   {pn} [on | off]: turn on/off the mode only admin can use bot"
-				+ "\n   {pn} noti [on | off]: turn on/off the notification when user is not admin use bot"
+			vi: " {pn} [on | off]: bật/tắt chế độ chỉ quản trị viên nhóm mới có thể sử dụng bot"
+				+ "\n {pn} noti [on | off]: bật/tắt thông báo khi người dùng không phải là quản trị viên nhóm sử dụng bot",
+			en: " {pn} [on | off]: turn on/off the mode only admin of group can use bot"
+				+ "\n {pn} noti [on | off]: turn on/off the notification when user is not admin of group use bot"
 		}
 	},
 
 	langs: {
 		vi: {
-			turnedOn: "Đã bật chế độ chỉ admin mới có thể sử dụng bot",
-			turnedOff: "Đã tắt chế độ chỉ admin mới có thể sử dụng bot",
-			turnedOnNoti: "Đã bật thông báo khi người dùng không phải là admin sử dụng bot",
-			turnedOffNoti: "Đã tắt thông báo khi người dùng không phải là admin sử dụng bot"
+			turnedOn: "Đã bật chế độ chỉ quản trị viên nhóm mới có thể sử dụng bot",
+			turnedOff: "Đã tắt chế độ chỉ quản trị viên nhóm mới có thể sử dụng bot",
+			turnedOnNoti: "Đã bật thông báo khi người dùng không phải là quản trị viên nhóm sử dụng bot",
+			turnedOffNoti: "Đã tắt thông báo khi người dùng không phải là quản trị viên nhóm sử dụng bot",
+			syntaxError: "Sai cú pháp, chỉ có thể dùng {pn} on hoặc {pn} off"
 		},
 		en: {
-			turnedOn: "Turned on the mode only admin can use bot",
-			turnedOff: "Turned off the mode only admin can use bot",
-			turnedOnNoti: "Turned on the notification when user is not admin use bot",
-			turnedOffNoti: "Turned off the notification when user is not admin use bot"
+			turnedOn: "Turned on the mode only admin of group can use 𝐒𝐡𝐢𝐏𝐮 𝐀𝐢 🤖💨",
+			turnedOff: "Turned off the mode only admin of group can use 𝐒𝐡𝐢𝐏𝐮 𝐀𝐢 🤖💨",
+			turnedOnNoti: "Turned on the notification when user is not admin of group use bot",
+			turnedOffNoti: "Turned off the notification when user is not admin of group use bot",
+			syntaxError: "Syntax error, only use {pn} on or {pn} off"
 		}
 	},
 
-	onStart: function ({ args, message, getLang }) {
+	onStart: async function ({ args, message, event, threadsData, getLang }) {
 		let isSetNoti = false;
 		let value;
+		let keySetData = "data.onlyAdminBox";
 		let indexGetVal = 0;
 
 		if (args[0] == "noti") {
 			isSetNoti = true;
 			indexGetVal = 1;
+			keySetData = "data.hideNotiMessageOnlyAdminBox";
 		}
 
 		if (args[indexGetVal] == "on")
@@ -53,17 +53,13 @@ module.exports = {
 		else if (args[indexGetVal] == "off")
 			value = false;
 		else
-			return message.SyntaxError();
+			return message.reply(getLang("syntaxError"));
 
-		if (isSetNoti) {
-			config.hideNotiMessage.adminOnly = !value;
-			message.reply(getLang(value ? "turnedOnNoti" : "turnedOffNoti"));
-		}
-		else {
-			config.adminOnly.enable = value;
-			message.reply(getLang(value ? "turnedOn" : "turnedOff"));
-		}
+		await threadsData.set(event.threadID, isSetNoti ? !value : value, keySetData);
 
-		fs.writeFileSync(client.dirConfig, JSON.stringify(config, null, 2));
+		if (isSetNoti)
+			return message.reply(value ? getLang("turnedOnNoti") : getLang("turnedOffNoti"));
+		else
+			return message.reply(value ? getLang("turnedOn") : getLang("turnedOff"));
 	}
 };
